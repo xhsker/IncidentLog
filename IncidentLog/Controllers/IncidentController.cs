bcb5b839e3f8d53ecd6abfcff88e23fe0f9f4e29
Tom Lake <xhsker@sbcglobal.net>
2019-05-02 10:10:33 -0500

    Add project files.

---
+++ IncidentLog/Controllers/IncidentController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IncidentLog.Models;
+
+namespace IncidentLog.Controllers
+{
+    public class IncidentController : Controller
+    {
+        private IncidentDbContext db = new IncidentDbContext();
+
+        // GET: Incident
+        public ActionResult Index()
+        {
+            var incidents = db.Incidents.Include(i => i.UnitType);
+            return View(incidents.ToList());
+        }
+
+        // GET: Incident/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Incident incident = db.Incidents.Find(id);
+            if (incident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(incident);
+        }
+
+        // GET: Incident/Create
+        public ActionResult Create()
+        {
+            ViewBag.Unit = new SelectList(db.UnitTypes, "Id", "Department");
+            return View();
+        }
+
+        // POST: Incident/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,IncidentDate,IncidentTime,Unit,Incident1,Summary")] Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Incidents.Add(incident);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Unit = new SelectList(db.UnitTypes, "Id", "Department", incident.Unit);
+            return View(incident);
+        }
+
+        // GET: Incident/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Incident incident = db.Incidents.Find(id);
+            if (incident == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Unit = new SelectList(db.UnitTypes, "Id", "Department", incident.Unit);
+            return View(incident);
+        }
+
+        // POST: Incident/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,IncidentDate,IncidentTime,Unit,Incident1,Summary")] Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(incident).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Unit = new SelectList(db.UnitTypes, "Id", "Department", incident.Unit);
+            return View(incident);
+        }
+
+        // GET: Incident/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Incident incident = db.Incidents.Find(id);
+            if (incident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(incident);
+        }
+
+        // POST: Incident/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Incident incident = db.Incidents.Find(id);
+            db.Incidents.Remove(incident);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IncidentLog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IncidentLog.MvcApplication" Language="C#" %>
